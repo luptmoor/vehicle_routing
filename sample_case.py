0b42ee3b484d4c2d52d81099ed3cd7c9fd95f3cf
luptmoor <lukas.uptmoor@web.de>
2024-11-04 11:43:46 +0100

    addeed gurobi solver structure

--- sample_case.py
+++ sample_case.py
@@ -2,26 +2,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Parameters
+# Scenario Parameters
 np.random.seed(42);
 MAP_SIZE = 100;
 GRID_STEPS = 10;
 N = 12;
+M = 3;
 MAX_DEMAND = 10;
 
 # Generate 100 x 100 km map
 side_array = np.arange(MAP_SIZE);
 x, y = np.meshgrid(side_array, side_array);
-coordinate_list = np.vstack([x.ravel(), y.ravel()]).T;
+coordinate_grid = np.vstack([x.ravel(), y.ravel()]).T;
 
 # Choose N map points as depot and customers
-chosen_coordinates = coordinate_list[np.random.choice(range(len(coordinate_list)), N, replace=False)];
-depot, customers = chosen_coordinates[0, :], chosen_coordinates[1:, :];
+nodes = coordinate_grid[np.random.choice(range(len(coordinate_grid)), N, replace=False)];
+depot, customers = nodes[0, :], nodes[1:, :];
 
 # Generate distance matrix
 distance_matrix = np.zeros((N, N));
 for i, j in [(i, j) for i in range(N) for j in range(N)]:
-    distance_matrix[i, j] = np.sqrt((chosen_coordinates[i, 0] - chosen_coordinates[j, 0])**2 + (chosen_coordinates[i, 1] - chosen_coordinates[j, 1])**2);
+    distance_matrix[i, j] = np.sqrt((nodes[i, 0] - nodes[j, 0])**2 + (nodes[i, 1] - nodes[j, 1])**2);
 
 
 # Generate demand list
@@ -46,3 +47,34 @@ plt.annotate('Depot', [x + 1 for x in depot]);
 
 plt.show();
 
+
+
+
+# Gurobi Model definition
+m = Model('Vehicle Routing');
+m.update();
+m.modelSense = GRB.MINIMIZE;
+
+# TODO: test
+xij = m.addVars(N, N, M, vtype = GRB.BINARY, name='x_ij');
+
+m.setObjective(sum(xij * distance_matrix)); 
+
+
+# Constraints
+
+# 1. Customers are only visited once by a vehicle
+
+# 2. Depot capacity is not exceeded
+
+# 3. Vehicles leave at depot
+
+# 4. Vehicles end at depot
+
+# 5. Vehicles move between customers
+
+# 6. Subtour elimination
+
+
+m.optimize();
+m.write('vehicle_routing.lp')
