0b0731feca34461e5948e8a7359c73a75a117962
luptmoor <lukas.uptmoor@web.de>
2024-11-14 17:37:25 +0100

    Objective function and 3 constraints added. Model currently infeasible

--- sample_case.py
+++ sample_case.py
@@ -4,63 +4,94 @@ import matplotlib.pyplot as plt
 from gurobipy import *
 
 # Scenario Parameters
-np.random.seed(45);
-MAP_SIZE = 100;
+np.random.seed(42);
+MAP_SIZE = 100; # km (in a square)
 GRID_STEPS = 10;
-N = 12;
-M = 3;
-MAX_DEMAND = 10;
+N = 8;
 
-MAX_VEHICLES = 4;
-VEHICLE_CAPS = [6, 15, 40];
-VEHICLE_SPEEDS = [170, 80, 40];
+MAX_VEHICLES = 5;
+VEHICLE_CAPACITIES = [6, 15, 40];
+VEHICLE_VELOCITIES = [170, 80, 40];
 
 # Generate 100 x 100 km map
 side_array = np.arange(MAP_SIZE);
 x, y = np.meshgrid(side_array, side_array);
 coordinate_grid = np.vstack([x.ravel(), y.ravel()]).T;
 
+
 # Choose N map points as depot and customers
 nodes = coordinate_grid[np.random.choice(range(len(coordinate_grid)), N, replace=False)];
 depot, customers = nodes[0, :], nodes[1:, :];
 
-# Generate distance matrix
+
+# Generate delivery fleet
+fleet_numbers = np.random.randint(0, MAX_VEHICLES, 3);  # number of vehicles for each type
+
+fleet_list = [fleet_numbers[i] * [i] for i in range(3)];  # give the type of all vehicles in a list for encoding
+fleet_list = [item for sublist in fleet_list for item in sublist]; # flatten the list
+
+velocity_list = [VEHICLE_VELOCITIES[x] for x in fleet_list];
+capacity_list = [VEHICLE_CAPACITIES[x] for x in fleet_list];
+
+fleet_capacity = sum(capacity_list); # Total capacity of fleet
+M = len(fleet_list); # Number of vehicles
+
+
+# Generate demand list (N-1)
+demand_list = [0] * (N-1);
+rem_fleet_capacity = fleet_capacity;
+max_demand = rem_fleet_capacity // (N-1) * 2;
+
+while rem_fleet_capacity > 0:
+    for i in range(N-1):
+        demand = min(np.random.randint(1, max_demand), rem_fleet_capacity);
+        demand_list[i] += demand;
+        rem_fleet_capacity -= demand;
+        if rem_fleet_capacity == 0: break;
+demand_list = [int(x) for x in demand_list];  # convert to pure integer list
+demand_list.insert(0, 0); # add 0 for demand of depot
+
+
+# Generate distance matrix (N x N)
 distance_matrix = np.zeros((N, N));
 for i, j in [(i, j) for i in range(N) for j in range(N)]:
     distance_matrix[i, j] = np.sqrt((nodes[i, 0] - nodes[j, 0])**2 + (nodes[i, 1] - nodes[j, 1])**2);
 
-# Generate delivery fleet
-fleet_numbers = np.random.randint(0, MAX_VEHICLES, 3);
-fleet_cap = sum(fleet_numbers * VEHICLE_CAPS);
+
+# Generate Traveltime Matrix (M x N x N)
+velocity_matrix = [np.full((N, N), 1) * velocity for velocity in velocity_list];
+traveltime_matrix = np.array([distance_matrix / velocities for velocities in velocity_matrix]);
 
 
-# Generate demand list
-demand_list = [0] * (N-1);
-rem_fleet_cap = fleet_cap;
-max_demand = rem_fleet_cap // (N-1) * 2;
 
-while rem_fleet_cap > 0:
-    for i in range(N-1):
-        demand = min(np.random.randint(1, max_demand), rem_fleet_cap);
-        demand_list[i] += demand;
-        rem_fleet_cap -= demand;
-        if rem_fleet_cap == 0: break;
-demand_list = [int(x) for x in demand_list];
+#___________________________________________________________________________________________________________________________________________
+
+
+
 
 # Report
 print();
 print('SCENARIO OVERVIEW');
 print();
 print(f'Fleet Report');
-print(f'Fast fixed-wings (C {VEHICLE_CAPS[0]}, V {VEHICLE_SPEEDS[0]}): {fleet_numbers[0]}');
-print(f'Quadrotors (C {VEHICLE_CAPS[1]}, V {VEHICLE_SPEEDS[1]}): {fleet_numbers[1]}');
-print(f'Cargo Blimps: (C {VEHICLE_CAPS[2]}, V {VEHICLE_SPEEDS[2]}) {fleet_numbers[2]}');
-print(f'Total capacity: {fleet_cap}');
+print(f'Total fleet size M: {M}')
+print(f'Fast fixed-wings (C {VEHICLE_CAPACITIES[0]}kg, V {VEHICLE_VELOCITIES[0]}km/h): {fleet_numbers[0]}');
+print(f'Quadrotors (C {VEHICLE_CAPACITIES[1]}kg, V {VEHICLE_VELOCITIES[1]}km/h): {fleet_numbers[1]}');
+print(f'Cargo Blimps (C {VEHICLE_CAPACITIES[2]}kg, V {VEHICLE_VELOCITIES[2]}km/h): {fleet_numbers[2]}');
+print(f'Fleet list for encoding: {fleet_list}');
+print(f'Velocity list [km/h]: {velocity_list}');
+print(f'Capacity list [kg]: {capacity_list}');
+print(f'Total capacity [kg]: {fleet_capacity}');
 print();
-print(f'A maximum demand of {max_demand} is used in distributing the demands.')
-print(f'{N-1} customers will be served with following demands:');
-print(f'{demand_list} ({sum(demand_list)} in total).')
+print(f'A maximum demand of {max_demand}kg is used in distributing the demands.')
+print(f'{N-1} customers will be served with following demands [kg]:');
+print(f'{demand_list} ({sum(demand_list)}kg in total).')
 print();
+print('Distance Matrix [km]');
+print(distance_matrix);
+print()
+print()
+
 
 # Visualise
 plt.scatter(depot.T[0], depot.T[1], s=30.0, c='b');
@@ -75,11 +106,14 @@ plt.grid();
 plt.xlabel('X Position [km]');
 plt.ylabel('Y Position [km]');
 for i in range(N-1):
-    plt.annotate(demand_list[i], [x + 1 for x in customers[i, :]]);
+    plt.annotate(demand_list[i+1], [x + 1 for x in customers[i, :]]);
 plt.annotate('Depot', [x + 1 for x in depot]);
 
 plt.show();
 
+
+
+#_________________________________________________________________________________________________________________________________________
 
 
 
@@ -88,21 +122,33 @@ m = Model('Vehicle Routing');
 m.update();
 m.modelSense = GRB.MINIMIZE;
 
-# TODO: test
-xij = m.addVars(N, N, M, vtype = GRB.BINARY, name='x_ij');
 
-m.setObjective(sum(xij * distance_matrix)); 
+# Binary decision variable (M x N x N)
+x = m.addVars(M, N, N, vtype = GRB.BINARY, name='x_ijk');
+
+# Total travel time [h] (i.e. hours that pilots have to be paid for, NOT duration of delivery)
+m.setObjective(sum(x[i, j, k] * traveltime_matrix[i, j, k] for i in range(M) for j in range(N) for k in range(N))); 
 
 
 # Constraints
 
-# 1. Customers are only visited once by a vehicle
+# 1. Customers k are only visited once by a vehicle
+for k in range(N-1):
+    m.addConstr(sum(x[i, j, k] for i in range(M) for j in range(N)) == 1);
 
-# 2. Depot capacity is not exceeded
+# 2. Vehicle i's capacity is not exceeded
+for i in range(M):
+    m.addConstr(sum(x[i, j, k] * demand_list[k] for j in range(N) for k in range(N)) <= capacity_list[i]);
 
-# 3. Vehicles leave at depot
+
+# 3. Vehicles i leave at depot 0 and do not come back
+for i in range(M):
+    m.addConstr(sum(x[i, 0, k] for k in range(N)) == 1); # yes: from 0 to k
+    m.addConstr(sum(x[i, j, 0] for j in range(N)) == 0); # no: from j to 0
 
 # 4. Vehicles end at depot
+#for i in range(M):
+    
 
 # 5. Vehicles move between customers
 
--- solver.py
+++
@@ -1,2 +0,0 @@
-
-def solve_model(N, M, )
