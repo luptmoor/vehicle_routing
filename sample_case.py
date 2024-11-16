a123f5a01413b57c2485b74bc43ea6bcfe728ef0
luptmoor <lukas.uptmoor@web.de>
2024-11-16 14:42:33 +0100

    More constraints added and format changed. Solution feasible and almost realistic now.

--- sample_case.py
+++ sample_case.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 from gurobipy import *
 
 # Scenario Parameters
-np.random.seed(42);
+np.random.seed(44);
 MAP_SIZE = 100; # km (in a square)
 GRID_STEPS = 10;
-N = 8;
 
-MAX_VEHICLES = 5;
-VEHICLE_CAPACITIES = [6, 15, 40];
-VEHICLE_VELOCITIES = [170, 80, 40];
+# Vehicle type characteristics [Fixed wing (fast but little capactiy), quadcopter (balanced), cargo blimp (slow but high capacity)]
+VEHICLE_CAPACITIES = [6, 15, 40]; # kg
+VEHICLE_VELOCITIES = [170, 80, 40]; # km/h
+
+N = 4; # number of nodes including depot
+M = 2; # number of vehicles
 
 # Generate 100 x 100 km map
 side_array = np.arange(MAP_SIZE);
@@ -20,47 +22,50 @@ coordinate_grid = np.vstack([x.ravel(), y.ravel()]).T;
 
 
 # Choose N map points as depot and customers
-nodes = coordinate_grid[np.random.choice(range(len(coordinate_grid)), N, replace=False)];
-depot, customers = nodes[0, :], nodes[1:, :];
-
+nodes = np.zeros((N+1, 2));
+nodes[:N, :] = coordinate_grid[np.random.choice(range(len(coordinate_grid)), N, replace=False)];
+customers, depot = nodes[:N-1, :], nodes[N-1, :];
+nodes[N, :] = depot; # duplicate depot node to mimic problem formulation from paper
 
 # Generate delivery fleet
-fleet_numbers = np.random.randint(0, MAX_VEHICLES, 3);  # number of vehicles for each type
+fleet_list = np.random.randint(0, 2, M);  # randomly decide vehicle type (0-2) for M vehicles
 
-fleet_list = [fleet_numbers[i] * [i] for i in range(3)];  # give the type of all vehicles in a list for encoding
-fleet_list = [item for sublist in fleet_list for item in sublist]; # flatten the list
+n_0, n_1, n_2 = 0, 0, 0; # Counters for vehicle types
+for i in fleet_list:
+    if i == 0: n_0 +=1;
+    elif i == 1: n_1 +=1;
+    elif i == 2: n_2 +=1;
 
-velocity_list = [VEHICLE_VELOCITIES[x] for x in fleet_list];
+velocity_list = [VEHICLE_VELOCITIES[x] for x in fleet_list]; # useful later
 capacity_list = [VEHICLE_CAPACITIES[x] for x in fleet_list];
 
 fleet_capacity = sum(capacity_list); # Total capacity of fleet
-M = len(fleet_list); # Number of vehicles
 
 
-# Generate demand list (N-1)
-demand_list = [0] * (N-1);
+# Generate demand list (N+1) (depot counted twice: as start and end)
+demand_list = [0] * (N+1);
 rem_fleet_capacity = fleet_capacity;
-max_demand = rem_fleet_capacity // (N-1) * 2;
+max_demand = rem_fleet_capacity // (N-1) * 3 // 2;
 
-while rem_fleet_capacity > 0:
-    for i in range(N-1):
-        demand = min(np.random.randint(1, max_demand), rem_fleet_capacity);
-        demand_list[i] += demand;
-        rem_fleet_capacity -= demand;
-        if rem_fleet_capacity == 0: break;
+for i in range(N-1):
+    demand = min(np.random.randint(1, max_demand), rem_fleet_capacity);
+    demand_list[i] += demand;
+    rem_fleet_capacity -= demand;
+    if rem_fleet_capacity == 0: break;
 demand_list = [int(x) for x in demand_list];  # convert to pure integer list
-demand_list.insert(0, 0); # add 0 for demand of depot
 
 
-# Generate distance matrix (N x N)
-distance_matrix = np.zeros((N, N));
-for i, j in [(i, j) for i in range(N) for j in range(N)]:
-    distance_matrix[i, j] = np.sqrt((nodes[i, 0] - nodes[j, 0])**2 + (nodes[i, 1] - nodes[j, 1])**2);
+
+# Generate distance matrix (N+1 x N+1)
+distance_matrix = np.zeros((N+1, N+1)); # [km]
+
+for i, j in [(i, j) for i in range(N+1) for j in range(N+1)]:
+    distance_matrix[i, j] = np.sqrt((nodes[i, 0] - nodes[j, 0])**2 + (nodes[i, 1] - nodes[j, 1])**2); # Pythagorean direct distance
 
 
-# Generate Traveltime Matrix (M x N x N)
-velocity_matrix = [np.full((N, N), 1) * velocity for velocity in velocity_list];
-traveltime_matrix = np.array([distance_matrix / velocities for velocities in velocity_matrix]);
+# Generate Traveltime Matrix (M x N+1 x N+1)
+velocity_matrix = [np.full((N+1, N+1), 1) * velocity for velocity in velocity_list]; # [km/h]
+traveltime_matrix = np.array([distance_matrix / velocities for velocities in velocity_matrix]); # [h]
 
 
 
@@ -69,15 +74,15 @@ traveltime_matrix = np.array([distance_matrix / velocities for velocities in vel
 
 
 
-# Report
+# Report for verification
 print();
 print('SCENARIO OVERVIEW');
 print();
 print(f'Fleet Report');
 print(f'Total fleet size M: {M}')
-print(f'Fast fixed-wings (C {VEHICLE_CAPACITIES[0]}kg, V {VEHICLE_VELOCITIES[0]}km/h): {fleet_numbers[0]}');
-print(f'Quadrotors (C {VEHICLE_CAPACITIES[1]}kg, V {VEHICLE_VELOCITIES[1]}km/h): {fleet_numbers[1]}');
-print(f'Cargo Blimps (C {VEHICLE_CAPACITIES[2]}kg, V {VEHICLE_VELOCITIES[2]}km/h): {fleet_numbers[2]}');
+print(f'Fast fixed-wings (C {VEHICLE_CAPACITIES[0]}kg, V {VEHICLE_VELOCITIES[0]}km/h): {n_0}');
+print(f'Quadrotors (C {VEHICLE_CAPACITIES[1]}kg, V {VEHICLE_VELOCITIES[1]}km/h): {n_1}');
+print(f'Cargo Blimps (C {VEHICLE_CAPACITIES[2]}kg, V {VEHICLE_VELOCITIES[2]}km/h): {n_2}');
 print(f'Fleet list for encoding: {fleet_list}');
 print(f'Velocity list [km/h]: {velocity_list}');
 print(f'Capacity list [kg]: {capacity_list}');
@@ -93,7 +98,7 @@ print()
 print()
 
 
-# Visualise
+# Visualise scenario without solution
 plt.scatter(depot.T[0], depot.T[1], s=30.0, c='b');
 plt.scatter(customers.T[0], customers.T[1], s=15.0, c='g');
 
@@ -106,10 +111,9 @@ plt.grid();
 plt.xlabel('X Position [km]');
 plt.ylabel('Y Position [km]');
 for i in range(N-1):
-    plt.annotate(demand_list[i+1], [x + 1 for x in customers[i, :]]);
+    plt.annotate(demand_list[i], [x + 1 for x in customers[i, :]]);
 plt.annotate('Depot', [x + 1 for x in depot]);
 
-plt.show();
 
 
 
@@ -123,37 +127,63 @@ m.update();
 m.modelSense = GRB.MINIMIZE;
 
 
-# Binary decision variable (M x N x N)
-x = m.addVars(M, N, N, vtype = GRB.BINARY, name='x_ijk');
+# Binary decision variable (M x N+1 x N+1) (N+1 because depot counted twice: depotstart and depotend)
+x = m.addVars(range(M), range(N+1), range(N+1), vtype = GRB.BINARY, name='x_ijk');
 
 # Total travel time [h] (i.e. hours that pilots have to be paid for, NOT duration of delivery)
 m.setObjective(sum(x[i, j, k] * traveltime_matrix[i, j, k] for i in range(M) for j in range(N) for k in range(N))); 
 
 
-# Constraints
+# Constraints:  Vehicle i goes from node j to k
 
-# 1. Customers k are only visited once by a vehicle
+# 1. Customers k are only visited once by a vehicle.
 for k in range(N-1):
     m.addConstr(sum(x[i, j, k] for i in range(M) for j in range(N)) == 1);
 
-# 2. Vehicle i's capacity is not exceeded
+# 2. Vehicle i's capacity is not exceeded.
 for i in range(M):
-    m.addConstr(sum(x[i, j, k] * demand_list[k] for j in range(N) for k in range(N)) <= capacity_list[i]);
+    m.addConstr(sum(x[i, j, k] * demand_list[k] for j in range(N) for k in range(N-1)) <= capacity_list[i]);
 
 
-# 3. Vehicles i leave at depot 0 and do not come back
+# 3. Vehicles i leave at depotStart N and do not come back
 for i in range(M):
-    m.addConstr(sum(x[i, 0, k] for k in range(N)) == 1); # yes: from 0 to k
-    m.addConstr(sum(x[i, j, 0] for j in range(N)) == 0); # no: from j to 0
+    m.addConstr(sum(x[i, N  -1, k] for k in range(N-1)) == 1); # yes: from N to k until N-1 (customers)
+    m.addConstr(sum(x[i, j, N  -1] for j in range(N)) == 0); # no: from j to N
 
-# 4. Vehicles end at depot
-#for i in range(M):
+# 4. Vehicles i do not leave at depotEnd N+1 but always come back
+for i in range(M):
+    m.addConstr(sum(x[i, N+1  -1, k] for k in range(N+1)) == 0);
+    m.addConstr(sum(x[i, j, N+1  -1] for j in range(N)) == 1);
     
 
-# 5. Vehicles move between customers
+# 5. Vehicles i move to next customer and do not come back
+for i in range(M):
+    for k in range(N-1):
+        m.addConstr(sum(x[i, j, k] for j in range(N) if i!=j) - sum(x[i, j, k] for j in range(N)) == 0);
+        m.addConstr(x[i, k, k] == 0);    # No entries on diagonal: vehicles do not stay at a node
+
 
 # 6. Subtour elimination
 
 
+
 m.optimize();
-m.write('vehicle_routing.lp')
+m.write('vehicle_routing.lp') # Only writes simplex problem formulation, not solution.
+
+
+
+#_______________________________________________________________________________________________________________________________________________________-
+
+
+# Convert solution to np.array for output print
+solution = np.zeros((M, N+1, N+1));
+for i in range(M):
+    for j in range(N+1):
+        for k in range(N+1):
+            solution[i, j, k] = x[i, j, k].X;
+
+print()
+print()
+print(solution);
+
+plt.show(); # only plots scenario, not solution
